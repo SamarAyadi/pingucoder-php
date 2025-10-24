4ef339087bc3363d9000b50889dcd9f038689116
Samar AYADI <ayadisamardev@gmail.com>
2025-10-24 12:19:03 +0100

    logic  if statement

--- index.php
+++ index.php
@@ -10,14 +10,29 @@
 <body>
 
   <?php
-      $a = 1;
-      $b = 2;
-      $c = "hi";
-      $d= "1";
+ 
+//  $ahmedAge = 20;
+//  $fatimaAge = 20;
 
-       var_dump( $a >= $b );
+//  if ($ahmedAge === $fatimaAge) {
+//   echo  "Ahmed is Older than fatima";
+//  } else {
+//   echo "Ahmed is Younger than fatima";
+//  }
 
-   ?>
+$case = 17;
+$case2 = 14;
+$case3 = 5;
+
+if ($case < $case2) {
+  echo "You can ship this case";
+}elseif( $case2 < $case3) {
+  echo "You can ship case2";
+}else {
+  echo "Sorry we can't ship anything to you ";
+}
+ 
+ ?>
 </body>
 
 </html>
